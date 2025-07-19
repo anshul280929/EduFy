fbeb370886f5d75a236601fc8e51a2b39c46730e
Anshul <anshulbhaskar50@gmail.com>
2025-07-19 10:26:32 +0530

    Programs Section and dark navabr

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './About.css';
+import aboutImage from '../../assets/about.png'; // Assuming you have an image for the about section
+import playIcon from '../../assets/play-icon.png'; // Assuming you have a play icon for the video
+const About = () => {
+  return (
+    <div className="about">
+      <div className='about-left'>
+        <img src={aboutImage} alt="" className='about-img'></img>
+        <img src={playIcon} alt="" className='play-icon'></img>
+      </div>
+      <div className='about-right'>
+        <h3>About University</h3>
+        <h2>
+            EduFy University: Shaping Futures Through Education
+        </h2>
+        <p>
+            Welcome to EduFy, a vibrant academic community dedicated to excellence in education, 
+            innovation, and leadership. Founded on the principles of knowledge, integrity, and service, our university 
+            has grown into a hub of learning that empowers students to explore their potential and shape the future.
+        </p>
+        <p>
+            At EduFy, we offer a diverse range of programs designed to meet the needs of our students and the demands of 
+            a rapidly changing world. Our faculty comprises distinguished scholars and industry experts who are committed 
+            to providing a transformative educational experience.
+        </p>
+        <p>
+            Our state-of-the-art facilities, coupled with a supportive and inclusive environment, foster creativity and 
+            collaboration. We encourage our students to engage in research, community service, and extracurricular activities 
+            that enhance their academic journey and personal growth.
+        </p>
+      </div>
+    </div>
+  );
+}
+export default About;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 const Navbar = () => {
+
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      (window.scrollY > 50)? setIsDark(true) : setIsDark(false);
+    });
+  }, []); 
+
   return (
-    <nav className='container'>
+    <nav className={`nav-container ${isDark ? 'dark-nav' : ''}`}>
       <img src={logo} alt="logo" className='logo' />
       <ul>
         <li>Home</li>
