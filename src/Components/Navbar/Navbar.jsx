7532da0eafbc4eef681f6ce5695bc432c4e2588d
Anshul <anshulbhaskar50@gmail.com>
2025-07-23 11:06:20 +0530

    React-scroll

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import { Link } from 'react-scroll';
 const Navbar = () => {
 
   const [isDark, setIsDark] = useState(false);
@@ -15,12 +16,12 @@ const Navbar = () => {
     <nav className={`nav-container ${isDark ? 'dark-nav' : ''}`}>
       <img src={logo} alt="logo" className='logo' />
       <ul>
-        <li>Home</li>
-        <li>Programs</li>
-        <li>About us</li>
-        <li>Campus</li>
-        <li>Services</li>
-        <li><button className='btn'>Contact us</button></li>
+        <li><Link to='hero' smooth={true} offset={0} duraion={500}>Home</Link></li>
+        <li><Link to='program' smooth={true} offset={-260} duraion={500}>Program</Link></li>
+        <li><Link to='about' smooth={true} offset={-150} duraion={500}>About us</Link></li>
+        <li><Link to='campus' smooth={true} offset={-260} duraion={500}>Campus</Link></li>
+        <li><Link to='testimonials' smooth={true} offset={-260} duraion={500}>Testimonials</Link></li>
+        <li><button className='btn'><Link to='contact' smooth={true} offset={-260} duraion={500}>Contact</Link></button></li>
       </ul>
     </nav>
   )
