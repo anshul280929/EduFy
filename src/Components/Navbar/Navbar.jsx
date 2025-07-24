87974ec0df9249316fbe25db4c8225d57f0e8c29
Anshul <anshulbhaskar50@gmail.com>
2025-07-24 11:07:13 +0530

    Small-screen-modification

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import menu from '../../assets/menu-icon.png'
 import { Link } from 'react-scroll';
 const Navbar = () => {
 
@@ -12,10 +13,15 @@ const Navbar = () => {
     });
   }, []); 
 
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu? setMobileMenu(false) : setMobileMenu(true);
+  }
+
   return (
     <nav className={`nav-container ${isDark ? 'dark-nav' : ''}`}>
       <img src={logo} alt="logo" className='logo' />
-      <ul>
+      <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
         <li><Link to='hero' smooth={true} offset={0} duraion={500}>Home</Link></li>
         <li><Link to='program' smooth={true} offset={-260} duraion={500}>Program</Link></li>
         <li><Link to='about' smooth={true} offset={-150} duraion={500}>About us</Link></li>
@@ -23,6 +29,7 @@ const Navbar = () => {
         <li><Link to='testimonials' smooth={true} offset={-260} duraion={500}>Testimonials</Link></li>
         <li><button className='btn'><Link to='contact' smooth={true} offset={-260} duraion={500}>Contact</Link></button></li>
       </ul>
+      <img src={menu} alt="logo" className='menu-icon' onClick={toggleMenu}/> 
     </nav>
   )
 }
